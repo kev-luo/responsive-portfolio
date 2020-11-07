046ff220eba9cfeacdbca9c8b37d6901ae7705fb
Kevin Luo <kvn.luo@gmail.com>
2020-11-07 00:58:47 -0500

    add ejs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Home' });
+})
+
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
+})
+
+router.get('/portfolio', (req, res) => {
+  res.render('portfolio', { title: 'Portfolio' });
 })
 
 module.exports = router;
